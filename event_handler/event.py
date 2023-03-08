5f2f7893a23dfee0a91b5e1c3716ea532a8ca732
Alex <alexander@nadjalin.com>
2023-03-08 09:22:46 +0100

    Started event_handler.py

---
+++ event_handler/event.py
@@ -0,0 +1,72 @@
+class Event:
+    """
+
+    Base class for events.
+    """
+    pass
+
+
+class NewBar(Event):
+    """
+
+    Market event indicates that a new day has passed and there is new market data.
+    """
+    def __init__(self,
+                 date: str):
+        self.type = 'BAR'
+        self.date = date
+
+    @property
+    def details(self) -> str:
+        """
+
+        Details for verbose logging.
+        :return: String for logging.
+        """
+        return 'New bar event [date: %s]' % self.date
+
+
+class Transaction(Event):
+    """
+
+    Transaction (buy or sell) event for a position in a portfolio.
+    """
+    def __init__(self,
+                 date: str,
+                 trans: transaction.Transaction):
+        self.type = 'TRANSACTION'
+        self.date = date
+        self.trans = trans
+
+    @property
+    def details(self) -> str:
+        """
+
+        Details for verbose logging.
+        :return: String for logging.
+        """
+        return 'Transaction event [date: %s, direction: %s, name: %s, quantity: %s, price: %s]' % (self.date,
+                                                                                                   self.trans.direction,
+                                                                                                   self.trans.name,
+                                                                                                   self.trans.quantity,
+                                                                                                   self.trans.price)
+
+
+class CalcSignal(Event):
+    """
+
+    Event indicating that we need to calculate the Strategy's signal requirements.
+    """
+    def __init__(self,
+                 date: str):
+        self.type = 'CALCSIGNAL'
+        self.date = date
+
+    @property
+    def details(self) -> str:
+        """
+
+        Details for verbose logging.
+        :return: String for logging.
+        """
+        return 'Calculate signal event [date: %s]' % self.date
---
+++ event_handler/event_handler.py
@@ -0,0 +1,81 @@
+import queue
+import event as ev
+
+
+class EventHandler:
+    """
+
+    Class for handling events.
+    """
+    def __init__(self,
+                 bt: Backtest,
+                 verbose=False):
+        """
+
+        Create empty queue.
+        :param bt: Backtest object.
+        :param verbose: Bool.
+        """
+        self.event_queue = queue.Queue()
+        self.bt = bt
+        self.verbose = verbose
+
+    def put_event(self,
+                  event: ev) -> None:
+        """
+
+        Put an event in the queue.
+        :param event: Event object.
+        :return: None.
+        """
+        self.event_queue.put(event)
+
+    def get_event(self) -> ev:
+
+        """
+
+        Get and remove next event from the queue.
+        :return: None.
+        """
+        return self.event_queue.get()
+
+    def is_empty(self) -> bool:
+        """
+
+        Check if queue is empty.
+        :return: Bool.
+        """
+        return self.event_queue.empty()
+
+    def handle_event(self) -> None:
+        """
+
+        Decide what to do with an event from the queue.
+        :return: None.
+        """
+        # Get and remove next event from queue.
+        e = self.get_event()
+
+        # New market data, new day.
+        if e.type == 'BAR':
+            # Update market values.
+            self.bt.pf.update_all_market_values(date=e.date,
+                                                market_data=self.bt.market)
+            if self.verbose:
+                print('INFO: ' + e.details)
+
+        # See if there is a trade signal.
+        if e.type == 'CALCSIGNAL':
+            # TODO Complete and add risk handling.
+            df = self.bt.market.select(columns=self.bt.pf.symbols,
+                                       start_date=e.date,
+                                       end_date=e.date)
+
+        if self.verbose:
+            print('INFO: ' + e.details)
+
+        # Transaction in portfolio.
+        elif e.type == 'TRANSACTION':
+            self.bt.pf.transact_security(trans=e.trans)
+            if self.verbose:
+                print('INFO: ' + e.details)
